2c83b087af524d63fc3e14af257f128f602fb17f
PC-20170813NFEO\Administrator <hcx_0711@hotmail.com>
2018-03-24 14:47:31 +0800

    1.0

---
+++ pages/index/index.js
@@ -0,0 +1,89 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    header:'这是标题',
+    content:'这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述',
+    listData: [
+      { "code": "标题", "text": "简介简介简介简介简介简介简介简介简介", "type": "../../public/img/10.jpg" },
+      { "code": "标题", "text": "简介简介简介简介简介简介简介简介简介", "type": "../../public/img/10.jpg" },
+      { "code": "标题", "text": "简介简介简介简介简介简介简介简介简介", "type": "../../public/img/10.jpg" },
+      { "code": "标题", "text": "简介简介简介简介简介简介简介简介简介", "type": "../../public/img/10.jpg" },
+      { "code": "标题", "text": "简介简介简介简介简介简介简介简介简介", "type": "../../public/img/10.jpg" },
+      { "code": "标题", "text": "简介简介简介简介简介简介简介简介简介", "type": "../../public/img/10.jpg" },
+      { "code": "标题", "text": "简介简介简介简介简介简介简介简介简介", "type": "../../public/img/10.jpg" }
+    ],
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    today:[{
+      name: 'div',
+      attrs: {
+        class: 'div_class',
+        style: 'line-height: 60px;'
+      },
+      children: [{
+        type: 'text',
+        text: '今日推荐'
+      }]
+    }],
+    body:{},
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  click:function(){
+    wx.getUserInfo({
+      withCredentials: true,
+      lang: '',
+      success: function(res) {
+        console.log(res);
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+    this.setData({msg:'hello word'})
+  },
+})
